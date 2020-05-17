01b43032d4ed0f0a5d2ab66b7c9c103bc4515597
captain <1328008364@qq.com>
2020-05-17 23:01:28 +0800

    friend实体类

---
+++ Entity/Friend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Friend
+    {
+        public string Name { set; get; }
+        public bool IsSaleMan { set; get; }
+    }
+}
---
+++ Logic/Map/MapUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Map
+{
+	public class MapUtil
+	{
+
+		public static void BreadthFirstSearch<T>(Hashtable hashtable,Type t)
+		{
+			Queue queue = new Queue();
+		}
+	}
+}
