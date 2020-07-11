3028a5fea1fad31976c3f5bd60ea1ac3f01de32e
captain <1328008364@qq.com>
2020-07-11 15:06:19 +0800

    公共构件

---
+++ Tools/Component/PublicComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Tools.Component
+{
+	public class PublicComponent
+	{
+		public Dictionary<string,object> Invoke(string classname,string methodName,object[] objs)
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+			dict.Add("Status", null);
+			dict.Add("Data", null);
+			dict.Add("ErrorMsg", null);
+			Type t = Type.GetType(classname);
+			Activator.CreateInstance(t);
+			Type[] types = new Type[objs.Length];
+			MethodInfo info = t.GetMethod(methodName, types);
+			info.Invoke();
+			return dict;
+		}
+	}
+}
