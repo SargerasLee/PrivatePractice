bdd5f072e2f4d5fed9d11efbf9db2c9a2c7ed794
captain <1328008364@qq.com>
2020-08-06 08:52:09 +0800

    加入全局配置文件

---
+++ Tools/GlobalConfig/GlobalConfigMgr.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Tools.Log;
+
+namespace Tools.GlobalConfig
+{
+	public sealed class GlobalConfigMgr
+	{
+		private static Dictionary<string, string> properties = new Dictionary<string, string>();
+		private static readonly object obj = new object();
+		private static bool flag = false;
+		static GlobalConfigMgr()
+		{
+			new LoggerFactory().GetInstance("test").Log("静态构造函数执行了");
+			XmlDocument xd = new XmlDocument();
+			xd.Load("GlobalConfig/GlobalConfig.xml");
+			FillTable(ref xd);
+		}
+
+		private static void FillTable(ref XmlDocument xd)
+		{
+			if (!flag)
+			{
+				lock (obj)
+				{
+					if (!flag)
+					{
+						XmlNodeList list = xd.SelectNodes("Configuration/properties/property");
+						foreach (XmlNode item in list)
+						{
+							properties.Add(item.Attributes["code"].Value, item.Attributes["value"].Value);
+						}
+						flag = true;
+					}
+				}
+			}
+		}
+
+		public static string GetProperty(string propertyCode)
+		{
+			return properties[propertyCode];
+		}
+	}
+}
---
+++ UnitTest/program/TestConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tools.GlobalConfig;
+namespace UnitTest.program
+{
+	[TestClass]
+	public class TestConfig
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			string zj = GlobalConfigMgr.GetProperty("zj");
+			string fy = GlobalConfigMgr.GetProperty("fy");
+			Console.WriteLine(zj);
+			Console.WriteLine(fy);
+		}
+	}
+}
