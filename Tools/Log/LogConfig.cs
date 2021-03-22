34202d3402664e7f041db79223e2ccbedec5a265
captain <1328008364@qq.com>
2021-03-22 16:44:37 +0800

    整合日志配置文件到全局配置

---
+++ Tools/Log/LogConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Log
+{
+	public class LogConfig
+	{
+		public string Code{ get; set; }
+		public string Name{ get; set; }
+		public string FullPath { get; set; }
+		public string Assembly { get; set; }
+		public string Class { get; set; }
+		public string Level{ get; set; }
+	}
+}
